4ddd7dcb9b409a32eb2a96ddddcb98ff2a253bae
4kauma4 <159747473+4kauma4@users.noreply.github.com>
2024-02-12 14:19:26 +0700

    Add files via upload

---
+++ LibraryRobots/LibraryRobots/Robots.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraryRobots
+{
+    public class Robots
+    {
+        private string name;
+        private int x;
+        private int y;
+        private int direction;
+        private Thread moveThread;
+
+        public Robots(string name, int x, int y)
+        {
+            this.name = name;
+            this.x = x;
+            this.y = y;
+            Random rnd = new Random();
+            this.direction = rnd.Next(0, 4);
+            moveThread = new Thread(Move);
+        }
+
+        public void StartMoving()
+        {
+            moveThread.Start();
+        }
+
+        private void Move()
+        {
+                switch (direction)
+                {
+                    case 0:
+                        y++;
+                        break;
+                    case 1:
+                        y--;
+                        break;
+                    case 2:
+                        x--;
+                        break;
+                    case 3:
+                        x++;
+                        break;
+                    default:
+                        break;
+                }
+
+            Thread.Sleep(1000);
+    
+        }
+
+        public string GetInfo()
+        {
+            string dir = "";
+            switch (direction)
+            {
+                case 0:
+                    dir = "Вперед";
+                    break;
+                case 1:
+                    dir = "Назад";
+                    break;
+                case 2:
+                    dir = "Налево";
+                    break;
+                case 3:
+                    dir = "Направо";
+                    break;
+                default:
+                    break;
+            }
+
+            return $"{name} {dir} ({x}, {y})";
+        }
+    }
+}
---
+++ LibraryRobots/Start/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LibraryRobots;
+
+Robots robot1 = new Robots("Робот 1", 200, 199);
+Robots robot2 = new Robots("Робот 2", 200, 199);
+
+robot1.StartMoving();
+robot2.StartMoving();
+
+
+for (int i = 0; i < 5; i++)
+{
+ 
+    Console.WriteLine(robot1.GetInfo());
+    Console.WriteLine(robot2.GetInfo());
+
+    Thread.Sleep(2000);
+    Console.WriteLine("Поток завершен!");
+}
+
